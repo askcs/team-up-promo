28b4be490d91ba2e11a94f05e3be5e65a83740c0
henk <henkiexxx4@gmail.com>
2015-03-23 13:20:27 +0100

    Added scrollReveal plugin to the bottom of the page

--- promo/js/main.js
+++ promo/js/main.js
@@ -1,1 +1,13 @@
-//alert(123);
+jQuery(function() {
+  /**
+   * Scrollreveal init
+   * @type {Window.scrollReveal}
+   */
+  window.sr = new scrollReveal({
+    reset: true,
+    move: '50px',
+    mobile: true
+  });
+
+  jQuery("[data-sr]").css("visibility", "hidden");
+});
