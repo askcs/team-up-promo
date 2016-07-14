13407a6f06a7b7139f09207b9edf4d8a98b5a37c
henkieee <henkiexxx4@gmail.com>
2016-07-14 16:26:36 +0200

    Added sendmail script

---
+++ sendMail.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+$contactName = isset($_REQUEST['contactName'])?$_REQUEST['contactName']:"";
+$contactEmail = isset($_REQUEST['contactEmail'])?$_REQUEST['contactEmail']:"";
+$contactSubject = isset($_REQUEST['contactSubject'])?$_REQUEST['contactSubject']:"";
+$contactMessage = isset($_REQUEST['message'])?$_REQUEST['message']:"";
+*/
+
+$contactEmail = "noreply <noreply@ask-cs.com>";
+
+$toEmail = "contact@teamtelefoon.nl";
+//$toEmail = "mma@ask-cs.com";
+
+$isDebug = true;
+
+function sendMail() {
+        global $toEmail;
+        $header = "FROM: $contactEmail\r\n";
+
+        //risky custom data
+        $query = explode('&', $_SERVER['QUERY_STRING'] );
+        $message = "";
+        foreach($query AS $q )
+        {
+                $kv = explode('=', $q );
+                $k = mysql_escape_string( $kv[0] );
+                $v = mysql_escape_string( urldecode($kv[1] ) ); // somewhere endlines get lost
+                $message .= $k.' = '.$v ."\n";
+        }
+
+        if( $message == '' )return "FALSE";
+
+        global $isDebug;
+        if( $isDebug)
+        {
+                mail("lvdinten@ask-cs.com", 'sendMail form', $message, $header);
+        }
+
+        if(mail($toEmail,'contact-formulier',$message,$header)){
+                return "TRUE";
+        }else{
+                return "FALSE";
+        }
+
+
+}
+
+// echo sendMail();
+
+
+        $header = "FROM: noreply<no-replyt@icaweb.nl>\r\n";
+        $header.= "Return-Path: <no-reply@icaweb.nl>\r\n";
+        mail( 'henk@teamtelefoon.nl', 'hello', 'testbody',  $header );
+?>
