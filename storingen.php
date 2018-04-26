08cbd9d73aa3fd790d69a6165d10138973934259
vijivasanthan <viji86@gmail.com>
2018-04-26 11:31:49 +0200

    storingen.json created and has the information of storing. storingenn php reads the storingen json and display error.

---
+++ storingen.php
@@ -0,0 +1,23 @@
+// Stupid CORS things..
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Request-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+<?php
+// Always return JSON
+header("Content-Type: application/json");
+
+$file = file_get_contents('storingen.json');
+
+if ($file == null || empty($file)) {
+    header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+    exit('{"error":"No source available. Contact system admin to fix it."}');
+}
+
+$json = json_decode($file);
+
+if ($file !== null){
+    echo json_encode($json);
+}
+?>
