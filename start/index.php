ea313b73ab01415b26c618cb79692d4e1d292885
Stefan Kroon <stefan@teamtelefoon.nl>
2018-08-20 12:05:24 +0200

    Updated the link to the Google-form starten-met-teamtelefoon after migration to new Google Domain

--- start/index.php
+++ start/index.php
@@ -1,3 +1,4 @@
 <?php
-	header("Location: https://docs.google.com/forms/d/e/1FAIpQLSdZbRfBOr5h_TPvOhM1Deq6FE8GK6btCIq0vujUKXbdt8z2zQ/viewform");
+	// Old: header("Location: https://docs.google.com/forms/d/e/1FAIpQLSdZbRfBOr5h_TPvOhM1Deq6FE8GK6btCIq0vujUKXbdt8z2zQ/viewform");
+	header("Location: https://docs.google.com/forms/d/1ysRSqdOYpcsDxZuI531e4Y6th04EOEgHOaBXhLrQpL8/viewform");
 ?>
